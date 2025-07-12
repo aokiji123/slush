5fa68463d226ff1d99510271c7a405880a70f0f4
Vyacheslav <slavik.adobe@gmail.com>
2025-07-12 13:21:04 +0200

    added neon db connection and User model

--- backend/Application/Services/UserService.cs
+++ backend/Application/Services/UserService.cs
@@ -1,8 +1,21 @@
+using Application.DTOs;
 using Application.Interfaces;
+using Domain.Interfaces;
 
 namespace Application.Services;
 
 public class UserService : IUserService
 {
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
     
+    public async Task<UserDto> GetUserAsync(Guid id)
+    {
+        var user = await _userRepository.GetByIdAsync(id);
+        return new UserDto(user.Id, user.Username, user.Email);
+    }
 }
