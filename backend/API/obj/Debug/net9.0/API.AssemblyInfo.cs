708eb0cf413a8e84d00c3689797f14c9ff80d323
Nikkkt <work.terpilovsky@gmail.com>
2025-08-06 17:44:54 +0300

    Added entities

--- backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4121ce785c2bfb03f85dcebdb68b2dca9479db90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41456fe07c9f9dddb77a74c769efd7de11444675")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ backend/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4121ce785c2bfb03f85dcebdb68b2dca9479db90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41456fe07c9f9dddb77a74c769efd7de11444675")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
