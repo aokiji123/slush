3acddfec48e536c67bf1586918d6d2a1098b2506
eleonorai <eellyyaa27@gmail.com>
2025-10-07 21:24:28 +0300

    v2

---
+++ frontend/src/components/poleInput/DescriptionPole.tsx
@@ -0,0 +1,29 @@
+import type { ChangeEvent } from "react";
+import './style.scss';
+
+interface DescriptionPoleI {
+  classTitle?: string,
+  classInput?: string,
+  placeholder: string,
+  title: string,
+  value: string,
+  onChange: (a: string) => void,
+  classContainer?: string
+  limitSymbol: number | null
+}
+
+export const DescriptionPole = ({ classTitle = '', classInput = '', placeholder, title, value, onChange, classContainer = '', limitSymbol }: DescriptionPoleI) => {
+  const onHandlerInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    onChange(value)
+  }
+  return <div className={'w-full flex flex-col gap-[8px] ' + classContainer}>
+    <div className='w-full flex justify-between items-center gap-[8px]'>
+      <h2 className={'text-[16px] font-bold text-[var(--color-background)] ' + classTitle} >{title}</h2>
+      {limitSymbol && <p className='text-[12px] text-[var(--color-background-25)]'>{value.length}/{limitSymbol}</p>}
+    </div>
+    <div className="w-full relative">
+      <textarea className={'w-full style-pole-input style-pole-input-big-simple ' + classInput} placeholder={placeholder} value={value} onChange={onHandlerInput} />
+    </div>
+  </div>
+}
---
+++ frontend/src/components/poleInput/FilePole.tsx
@@ -0,0 +1,43 @@
+import type { ChangeEvent } from "react";
+import './style.scss';
+
+interface FilePoleI {
+  classTitle?: string,
+  title: string,
+  value: string,
+  onChange: (a: string) => void,
+  classContainer?: string
+}
+
+export const FilePole = ({ classTitle = '', title, value, onChange, classContainer = '' }: FilePoleI) => {
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      // console.log(reader.result);
+      if (typeof reader.result === 'string') {
+        onChange(reader.result);
+      }
+    };
+    reader.readAsDataURL(file);
+  };
+  return <div className={'w-full flex flex-col gap-[8px] ' + classContainer}>
+    {title && <div className='w-full flex justify-between items-center gap-[8px]'>
+      <h2 className={'text-[16px] font-bold text-[var(--color-background)] ' + classTitle} >{title}</h2>
+    </div>}
+    <div className="style-pole-file-c w-full rounded-[12px] gap-[12px] flex justify-center items-center relative h-[300px] bg-[var(--color-background-14)] flex-wrap hover:border-[var(--color-background-50)] cursor-pointer">
+      {
+        value === '' &&
+        <>
+          <p className="text-[16px] text-[var(--color-background-25)] text-center">Перетягніть файл сюди або</p>
+          <button className="rounded-[20px] p-[9px] pl-[20px] pr-[20px] text-[var(--color-background)] bg-[var(--color-background-16)]"><span className="mt-[2px] flex">Завантажити</span></button>
+        </>}
+      {value
+        ? <img src={value} alt="" className="absolute h-full opacity-100" />
+        : <input onChange={handleFileChange} type="file" className="w-full absolute h-full opacity-0" />
+      }
+    </div>
+  </div>
+}
